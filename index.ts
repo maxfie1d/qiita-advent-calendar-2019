739f6b7add54c46b5a326dc91e6f9c0492edc7ce
Maxfield Walker <maxfield.walker@gmail.com>
2019-11-26 12:37:09 +0900

    Add curry

--- index.ts
+++ index.ts
@@ -5,6 +5,15 @@ function trueOrFalse() {
   return true;
 }
 
+/**
+ *  __  __             _            ____                 _ _
+|  \/  | __ _ _   _| |__   ___  |  _ \ ___  ___ _   _| | |_
+| |\/| |/ _` | | | | '_ \ / _ \ | |_) / _ \/ __| | | | | __|
+| |  | | (_| | |_| | |_) |  __/ |  _ <  __/\__ \ |_| | | |_
+|_|  |_|\__,_|\__, |_.__/ \___| |_| \_\___||___/\__,_|_|\__|
+              |___/
+ */
+
 const myMaybe: Maybe<number> = trueOrFalse()
   ? maybe.Just(123)
   : maybe.Nothing();
@@ -31,6 +40,14 @@ myResult.matchWith({
   }
 });
 
+/**
+ *  ___                           _        _     _
+|_ _|_ __ ___  _ __ ___  _   _| |_ __ _| |__ | | ___
+ | || '_ ` _ \| '_ ` _ \| | | | __/ _` | '_ \| |/ _ \
+ | || | | | | | | | | | | |_| | || (_| | |_) | |  __/
+|___|_| |_| |_|_| |_| |_|\__,_|\__\__,_|_.__/|_|\___|
+ */
+
 const array = [1, 2, 3];
 // Mutable
 array.push(4);
@@ -40,3 +57,20 @@ const array0 = R.prepend(0, array);
 
 console.log(array5); // => [1, 2, 3, 4, 5]
 console.log(array0); // => [0, 1, 2, 3, 4]
+
+/**
+ *   ____
+ / ___|   _ _ __ _ __ _   _
+| |  | | | | '__| '__| | | |
+| |__| |_| | |  | |  | |_| |
+ \____\__,_|_|  |_|   \__, |
+                      |___/
+ */
+
+const add3 = (a: number, b: number, c: number) => a + b + c;
+const curriedAdd = R.curry(add3);
+
+console.log(add3(1, 2, 3)); // => 6
+console.log(curriedAdd(1)(2, 3)); // => 6
+console.log(curriedAdd(1, 2)(3)); // => 6
+console.log(curriedAdd(1)(2)(3)); // => 6
